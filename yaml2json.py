0d4ea1b8fb1f098770165b566b4bb9079355b1c9
russell <m19825830647@163.com>
2021-04-08 16:55:24 +0800

    add yaml2json

---
+++ yaml2json.py
@@ -0,0 +1,13 @@
+import yaml
+
+yaml_path=r'test.yaml'
+json_path=r'test.json'
+def yaml2json(yaml_path:str,json_path:str):
+    with open(yaml_path, 'r', encoding='utf-8') as f:
+        content = f.read()
+    dict_content = yaml.load(content, Loader=yaml.FullLoader)
+    # print(dict_content)
+    with open(json_path, 'w+', encoding='utf-8') as f:
+        json.dump(dict_content, f)
+
+
