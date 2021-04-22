19b7099e49bd418c61f6b8cd760fd1694888930a
russell <m19825830647@163.com>
2021-04-22 15:22:24 +0800

    更新处理文件函数

---
+++ common/handleIni.py
@@ -0,0 +1,42 @@
+import configparser
+from configparser import ConfigParser
+from collections import defaultdict
+
+
+class HnadleIni:
+
+    def __init__(self, path=None) -> None:
+        '''
+        "session":[(k,v),
+                    (k,v)]
+                    }
+        '''
+        self.data = {}
+        if path is not None:
+            configp = ConfigParser()
+            configp.read(path)
+            for session in configp.sections():
+                self.data[session] = configp.items(session)
+
+    def load_ini(self, ini_path: str) -> dict:
+
+        configp = ConfigParser()
+        configp.read(ini_path)
+        for session in configp.sections():
+            self.data[session] = configp.items(session)
+
+
+
+    def write_to_ini(ini_path: str, data: dict):
+        '''
+            "session":[(k,v),
+                        (k,v)]
+                        }
+        '''
+        conf = ConfigParser()
+        for session, items in data:
+            conf.add_section(session)
+            for value in items:
+                conf.set(session, *value)
+        with open(ini_path,'w',encoding='utf-8') as f:
+            conf.write(f)
--- common/handleYaml.py
+++ common/handleYaml.py
@@ -12,14 +12,24 @@ from handlefile import read_file
 from attrdict import AttrDict  # 按照属性访问字典
 
 
-def read_yaml(path: str) -> list:
-    content = read_file(path)
-    yaml_data = yaml.load_all(content)
+class HandleYaml:
 
-    return [AttrDict(data) for data in yaml_data]
+    def __init__(self, path=None) -> None:
+        if path is None:
+            self.data=[]
+        else:
+            content = read_file(path)
+            yaml_data = yaml.safe_load_all(content)
+            self.data=[ AttrDict(data) for data in yaml_data ]
 
 
-def write_yaml(full_data: list, path: str):
-    with open(path, 'a+', encoding='utf-8') as f:
-        f.write('---\n')
-        yaml.dump_all(full_data, f)
+    def load_yaml(self,path: str) -> list:
+        content = read_file(path)
+        yaml_data = yaml.safe_load_all(content)
+        self.data= [ AttrDict(data) for data in yaml_data ]
+
+
+    def write_yaml(self, path: str):
+        with open(path, 'a+', encoding='utf-8') as f:
+            f.write('---\n')
+            yaml.dump_all([ dict(content) for content in self.data ], f)
