c5f9c8b532ae0ed23e87a7ad6542d9fa9e602ff7
russell <m19825830647@163.com>
2021-04-27 13:00:27 +0800

    change dir

---
+++ test/testSQL3.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+conn=sqlite3.connect('test.db')
+cursor=conn.cursor()
+
+
+cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
+
+cursor.execute('insert into user (id, name) values (\'1\', \'Michael\')')
+
+print(f'插入的行数: {cursor.rowcount}')
+
+
+cursor.close()
+conn.commit()
+
+conn.close()
